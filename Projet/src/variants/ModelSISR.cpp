a7c524d8ec415556fd78397b5693263ebb4ed8a0
sd-projet <99795261+sd-projet@users.noreply.github.com>
2023-01-13 14:56:48 +0100

    Update ModelSISR.cpp

--- Projet/src/variants/ModelSISR.cpp
+++ Projet/src/variants/ModelSISR.cpp
@@ -45,7 +45,8 @@ ModelSISR::~ModelSISR()
 {
 }
 
-// modele ou un individu qui a été infecte peut à nouveau devenir susceptible
+// Resolution du modele ou un individu qui a Ã©tÃ© infecte peut Ã  nouveau devenir susceptible 
+// SISR : Suscepties Infected Suscepties Recovered
 void ModelSISR::resoudSISR()
 {
     N1 = 1000;   // population
@@ -56,76 +57,73 @@ void ModelSISR::resoudSISR()
     gama1 = 0.1; // taux de guerison
 
     nu1 = 0.009; // taux de naissance
-    mu1 = 0.2;   // taux de mortalité
+    mu1 = 0.2;   // taux de mortalitÃ©
 
-    vector<double> S(nday1); // susceptible
-    vector<double> I(nday1); // infecte
-    vector<double> R(nday1); // recovered
-    vector<double> t;        // temps
+    vector<double> S(nday); // Suscepties
+    vector<double> I(nday); // infecte
+    vector<double> R(nday); // recovered
+    vector<double> t;       // temps
 
     I.at(0) = 0.001;     // Initial infective proportion
-    S.at(0) = N1 - I[0]; // initial susceptible
-    R.at(0) = beta1 / (mu1 + gama1);
-    // R.at(0) = 8 / 3;
+    S.at(0) = N - I[0]; // initial susceptible
+    R.at(0) = beta / (mu + gamma);
 
-    //  Define the ODE’s of the model and solve numerically by Euler’s method:
+    // Definir les ODE du modele et les resoudre numeriquement par la methode d'Euler
 
-    for (double i = 0; i < nday1; i += dt1)
+    for (double i = 0; i < nday; i += dt)
     {
         t.push_back(i);
 
-        S[i + 1] = S[i] + ((-beta1 / N1) * (S[i] * I[i]) + (mu1 * I[i])) * dt1;
-        I[i + 1] = I[i] + ((beta1 / N1) * S[i] * I[i] - (mu1 + gama1) * I[i]) * dt1;
-        R[i + 1] = R[i] + (gama1 * I[i]) * dt1;
+        S[i + 1] = S[i] + ((-beta / N) * (S[i] * I[i]) + (mu * I[i])) * dt;
+        I[i + 1] = I[i] + ((beta / N) * S[i] * I[i] - (mu + gamma) * I[i]) * dt;
+        R[i + 1] = R[i] + (gamma * I[i]) * dt;
 
-        if (S[i + 1] < 0 || S[i + 1] > N1)
+        if (S[i + 1] < 0 || S[i + 1] > N)
         {
             cout << "Attention ! " << endl;
-            cout << "Le nombre des Susceptible est soit inferieur a 0 soit superieur a la population totale." << endl;
-            // break;
+            cout << "Le nombre des Suscepties est soit inferieur a 0 soit superieur a la population totale." << endl;
         }
-        else if (I[i + 1] < 0 || I[i + 1] > N1)
+        else if (I[i + 1] < 0 || I[i + 1] > N)
         {
             cout << "Attention ! " << endl;
-            cout << "Le nombre des Infecte est soit inferieur a 0 soit superieur a la population totale." << endl;
-            // break;
+            cout << "Le nombre des Infected est soit inferieur a 0 soit superieur a la population totale." << endl;
         }
-        else if (R[i + 1] < 0 || R[i + 1] > N1)
+        else if (R[i + 1] < 0 || R[i + 1] > N)
         {
             cout << "Attention ! " << endl;
-            cout << "Le nombre des Guerri est soit inferieur a 0 soit superieur a la population totale." << endl;
-            // break;
+            cout << "Le nombre des Recovered est soit inferieur a 0 soit superieur a la population totale." << endl;
         }
-        cout << "Jour numero : " << i + 1 << "  Sucesptible : " << S[i + 1] << "  Infected : " << I[i + 1] << "  Recovered : " << R[i + 1] << endl;
+        cout << "Jour numero : " << i + 1 << "  Suscepties : " << S[i + 1] << "  Infected : " << I[i + 1] << "  Recovered : " << R[i + 1] << endl;
     }
-    Plot2D plot;
-    plot.size(700, 700);
+    
+    Plot2D plot; // creation d'un graphe
 
-    // This disables the deletion of the created gnuplot script and data file.
-    plot.autoclean(false);
-    plot.xlabel("Temps");
-    plot.ylabel("Population");
-    // Change its palette
-    plot.palette("dark2");
+    plot.autoclean(false); // desactive la suppression du script gnuplot cree et du fichier de donnees
+    plot.palette("dark2"); // changement de palette
 
-    // Plot two functions
+    plot.xlabel("Temps"); // legende de l'axe x
+    plot.ylabel("Population"); // legende de l'axe y
+
+    // trace les courbes
     plot.drawCurve(t, S).label("Suscepties");
     plot.drawCurve(t, I).label("Infected");
     plot.drawCurve(t, R).label("Recovered");
+
+    // legende des courbes
     plot.legend()
         .atOutsideRightBottom()
         .displayVertical();
 
-    // Create figure to hold plot
+    // Creation d'une figure qui contient les courbes cree
     Figure fig = { {plot} };
-    // Create canvas to hold figure
+
+    // Creation d'un canvas pour afficher la figure
     Canvas canvas = { {fig} };
-    fig.title("Modele SISR");
+    canvas.size(750, 750); //taille du canvas
 
-
-    // Show the plot in a pop-up window
+    // Affichage du canvas dans une fenetre
     canvas.show();
 
-    // Save the plot to a PDF file
+    // Graphe enregistre dans le dossier graphs
     canvas.save("../graphs/modelSISR.pdf");
 }
